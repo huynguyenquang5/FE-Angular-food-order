abfa4420699e930f249fb41b84992e0fb9c137dc
Quang Huy Nguyen <huynguyenquang5@gmail.com>
2023-02-15 06:47:48 +0700

    feature-2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,13 @@
 export const environment = {
   production: false,
-
   apiUrl: 'http://localhost:8080',
   firebaseConfig: {
-    apiKey: "AIzaSyCEalnb3pig1Fzu_VDf-ggMtsLVpk-xDvM",
-    authDomain: "quanghuynguyen-1dd91.firebaseapp.com",
-    projectId: "quanghuynguyen-1dd91",
-    storageBucket: "quanghuynguyen-1dd91.appspot.com",
-    messagingSenderId: "1005868156878",
-    appId: "1:1005868156878:web:1d07e957f43d8d96f18d9f",
-    measurementId: "G-F6G2WY6DG3"
-  }
-
-  // firebaseConfig: {
-  //   apiKey: "AIzaSyCBo1K205P9kgF1sA2z4tR4fjAxjjEZAE0",
-  //   authDomain: "case-study-63dd5.firebaseapp.com",
-  //   projectId: "case-study-63dd5",
-  //   storageBucket: "case-study-63dd5.appspot.com",
-  //   messagingSenderId: "1084622975652",
-  //   appId: "1:1084622975652:web:8fa867bacdd328666cac31",
-  //   measurementId: "G-8WY0VTVQP3"
-  // },
-
-
-
+    apiKey: "AIzaSyCBo1K205P9kgF1sA2z4tR4fjAxjjEZAE0",
+    authDomain: "case-study-63dd5.firebaseapp.com",
+    projectId: "case-study-63dd5",
+    storageBucket: "case-study-63dd5.appspot.com",
+    messagingSenderId: "1084622975652",
+    appId: "1:1084622975652:web:8fa867bacdd328666cac31",
+    measurementId: "G-8WY0VTVQP3"
+  },
 };
