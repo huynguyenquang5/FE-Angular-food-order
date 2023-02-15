ff86672cfc6721443995216793fad09ccdae0770
NguyenTu <nngoctu004@gmail.com>
2023-02-15 16:18:42 +0700

    menu cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DetailStoreComponent} from "./detail-store/detail-store.component";
 import {HomeBodyComponent} from "./home/home-body/home-body.component";
-import {AdminPartnerPendingComponent} from "./admin/admin-partner-pending/admin-partner-pending.component";
-import {AdminPartnerComponent} from "./admin/admin-partner/admin-partner.component";
-import {AdminMerchantPendingComponent} from "./admin/admin-merchant-pending/admin-merchant-pending.component";
-import {AdminMerchantComponent} from "./admin/admin-merchant/admin-merchant.component";
 import {AdminUserComponent} from "./admin/admin-user/admin-user.component";
+import {AdminMerchantComponent} from "./admin/admin-merchant/admin-merchant.component";
+import {AdminMerchantPendingComponent} from "./admin/admin-merchant-pending/admin-merchant-pending.component";
+import {AdminPartnerComponent} from "./admin/admin-partner/admin-partner.component";
+import {AdminPartnerPendingComponent} from "./admin/admin-partner-pending/admin-partner-pending.component";
+
 
 const routes: Routes = [
   {
