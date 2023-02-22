0fd92dc9bafab99be2036f0349d27bfdc2b161db
Quang Huy Nguyen <huynguyenquang5@gmail.com>
2023-02-22 17:24:36 +0700

    fix admin

--- src/app/admin/admin-merchant/admin-merchant.component.ts
+++ src/app/admin/admin-merchant/admin-merchant.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {AdminService} from "../../service/admin/admin.service";
 import {User} from "../../model/user/user";
+import {Address} from "../../model/user/address";
+import {Store} from "../../model/store/store";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-admin-merchant',
@@ -8,12 +11,19 @@ import {User} from "../../model/user/user";
   styleUrls: ['./admin-merchant.component.css']
 })
 export class AdminMerchantComponent implements OnInit {
+  user!: User;
+  store!: Store;
   users: User[] = [];
+  addresses: Address[] = [];
   id: number | undefined;
   status: number | undefined;
-  constructor(private adminService: AdminService) {
+  constructor(private adminService: AdminService,
+              private router: Router) {
   }
   ngOnInit() {
+    this.getAllUsers();
+  }
+  getAllUsers() {
     this.adminService.findAllUsers().subscribe(users => {
       for (let i = 0; i < users.length; i++) {
         if (users[i].status == 0 || users[i].status == 1){
@@ -26,6 +36,17 @@ export class AdminMerchantComponent implements OnInit {
       }
     });
   }
+  getOneUser(id: number) {
+    this.adminService.findOneUser(id).subscribe((data) => {
+      this.user = data;
+    })
+    this.adminService.findAllAddressesByUserId(id).subscribe((data) => {
+      this.addresses = data;
+    })
+    this.adminService.findStoreByUserId(id).subscribe((data) => {
+      this.store = data;
+    })
+  }
   getIdAndStatus(id: number | undefined, status: number | undefined) {
     this.id = id;
     this.status = status;
@@ -34,6 +55,7 @@ export class AdminMerchantComponent implements OnInit {
     this.adminService.activeBlockUser(this.id, this.status).subscribe( () => {
       window.location.reload();
     }, error => {
+      alert("Something wrong")
       console.log(error);
     })
   }
