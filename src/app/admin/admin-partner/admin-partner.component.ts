3306cc553cd47cb1d4d41a64cf044c772008ed02
Quang Huy Nguyen <huynguyenquang5@gmail.com>
2023-02-22 17:26:13 +0700

    fix admin

--- src/app/admin/admin-partner/admin-partner.component.ts
+++ src/app/admin/admin-partner/admin-partner.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from "../../model/user/user";
 import {AdminService} from "../../service/admin/admin.service";
+import {Store} from "../../model/store/store";
+import {Address} from "../../model/user/address";
 
 @Component({
   selector: 'app-admin-partner',
@@ -8,12 +10,18 @@ import {AdminService} from "../../service/admin/admin.service";
   styleUrls: ['./admin-partner.component.css']
 })
 export class AdminPartnerComponent implements OnInit {
+  user!: User;
+  store!: Store;
   users: User[] = [];
+  addresses: Address[] = [];
   id: number | undefined;
   status: number | undefined;
   constructor(private adminService: AdminService) {
   }
   ngOnInit() {
+    this.getAllUsers();
+  }
+  getAllUsers() {
     this.adminService.findAllUsers().subscribe(users => {
       for (let i = 0; i < users.length; i++) {
         if (users[i].status == 0 || users[i].status == 1){
@@ -26,7 +34,17 @@ export class AdminPartnerComponent implements OnInit {
       }
     });
   }
-
+  getOneUser(id: number) {
+    this.adminService.findOneUser(id).subscribe((data) => {
+      this.user = data;
+    })
+    this.adminService.findAllAddressesByUserId(id).subscribe((data) => {
+      this.addresses = data;
+    })
+    this.adminService.findStoreByUserId(id).subscribe((data) => {
+      this.store = data;
+    })
+  }
   getIdAndStatus(id: number | undefined, status: number | undefined) {
     this.id = id;
     this.status = status;
--- src/app/service/admin/admin.service.ts
+++ src/app/service/admin/admin.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../../model/user/user";
 import {environment} from "../../../environments/environment";
+import {Address} from "../../model/user/address";
+import {Store} from "../../model/store/store";
 
 const API_URL = `${environment.apiUrl}`;
 @Injectable({
@@ -12,6 +14,15 @@ export class AdminService {
   constructor(private http: HttpClient) { }
   findAllUsers(): Observable<User[]> {
     return this.http.get<User[]>(`${API_URL}/admin/users`)
+  }
+  findOneUser(id: number): Observable<User> {
+    return this.http.get<User>(`${API_URL}/admin/users/${id}`)
+  }
+  findAllAddressesByUserId(id: number): Observable<Address[]> {
+    return this.http.get<Address[]>(`${API_URL}/address/${id}`)
+  }
+  findStoreByUserId(id: number): Observable<Store> {
+    return this.http.get<Store>(`${API_URL}/store/user/${id}`);
   }
   activeBlockUser(id: number | undefined, status: number | undefined) : Observable<any> {
     // @ts-ignore
